f0bfb583211bfd30fb540b1aa029b0a81ff2cef8
MesminN <mesmin.ngoulo@gmail.com>
2023-02-12 22:21:10 +0100

    Update main.py

--- EDR_CVE_Scrapper/main.py
+++ EDR_CVE_Scrapper/main.py
@@ -1,50 +1,82 @@
 # This is a sample Python script.
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def browse_for_data(url):
+    driver = webdriver.Edge()
+    driver.get(url)
+
+    html = driver.page_source
+
+    driver.quit()
+    return html
+
+
+def retrieve_product_name(b_soup):
+    content_div = b_soup.find('div', attrs={'id': 'contentdiv'})
+    a_tags = content_div.find('h1').find_all('a')
+
+    name = ''
+    for ele in a_tags:
+        name += ' '
+        name += ele.text.strip()
+
+    return name.strip()
+
+
+def retrieve_data_from_table(b_soup, table_id, table_class, tr_class):
+    data = []
+    table_attrs = {}
+    if table_id:
+        table_attrs['id'] = table_id
+
+    if table_class:
+        table_attrs['class'] = table_class
+
+    table = b_soup.find('table', attrs=table_attrs)
+
+    tr_attrs = {}
+    if tr_class:
+        tr_attrs['class'] = tr_class
+
+    # Read headers
+
+    # Read values
+    rows = table.find_all('tr', attrs=tr_attrs)
+    for row in rows:
+        cols = row.find_all('td')
+        selected_cols = []
+        for index, ele in enumerate(cols):
+            if index >= 8:
+                break
+            elif index != 2 and index != 3:
+                selected_cols.append(ele.text.strip())
+
+        data.append([ele for ele in selected_cols])
+    return data
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi("Corentin")
+    urls = ["https://www.cvedetails.com/vulnerability-list/vendor_id-76/product_id-34197/year-2017/"
+            "Symantec-Advanced-Threat-Protection.html",
+            "https://www.cvedetails.com/vulnerability-list/vendor_id-29474/product_id-132532/year-2023/"
+            "Cybereason-Endpoint-Detection-And-Response.html",
+            "https://www.cvedetails.com/vulnerability-list/vendor_id-16/product_id-114009/year-2022/"
+            "Cisco-Secure-Endpoint.html",
+            "https://www.cvedetails.com/vulnerability-list/vendor_id-2032/year-2021/Opentext.html"]
 
-    # Utilisation de la bibliothèque requests pour réaliser la requête HTTP sur le site web gartner.com
-    response = requests.get("https://www.gartner.com/reviews/market/endpoint-detection-and-response-solutions")
+    vuln_dict = {}
 
-    # Vérification de la réponse HTTP
-    if response.status_code == 200:
-        # Si la réponse est OK, parsing du corps de la réponse avec BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
-        print(soup.prettify())
-    else:
-        # Si la réponse n'est pas OK, affichage d'un message d'erreur
-        print("Error: Response code {}".format(response.status_code))
+    for url in urls:
+        html_content = browse_for_data(url)
 
-    # Utilisation de Selenium pour naviguer sur le site web cvedetails.com
-    driver = webdriver.Edge()
-    driver.get("https://www.cvedetails.com/")
+        soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Recherche de la chaîne "Test" sur le site web cvedetails.com
-    search_box = driver.find_element_by_name("CVE")
-    search_box.send_keys("Test")
-    search_box.send_keys(Keys.RETURN)
+        # print(soup.prettify())
+        data = retrieve_data_from_table(soup, 'vulnslisttable', '', 'srrowns')
+        vuln_dict[retrieve_product_name(soup)] = data
 
-    # Affichage du corps HTML de la page résultante
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    print(soup.prettify())
-
-    # Fermeture du navigateur
-    driver.quit()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(vuln_dict)
