49c45c5e5b19f9eae493466b30727c0f9fadcf09
masknetto <121174232+masknetto@users.noreply.github.com>
2023-02-13 18:26:11 +0100

    legend

--- EDR_CVE_Scrapper/firstgraph.py
+++ EDR_CVE_Scrapper/firstgraph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
 def gravite_moyenne_par_solution(data):
@@ -12,9 +13,15 @@ def gravite_moyenne_par_solution(data):
         sum = 0
         for line in matrix:
             mean += float(line[5])
-            sum +=1
+            sum += 1
         mean /= sum
         cves.append(mean)
-    plt.bar(range(len(data)), cves)
-    plt.legend(names)
+
+    cmap = cm.get_cmap('viridis', len(cves))
+    for i, (name, cve) in enumerate(zip(names, cves)):
+        plt.bar(i, cve, color=cmap(i), label=name)
+    plt.xticks(range(len(data)))
+    plt.xlabel("Solution")
+    plt.ylabel("Gravité Moyenne")
+    plt.legend()
     plt.show()
--- EDR_CVE_Scrapper/thirdgraph.py
+++ EDR_CVE_Scrapper/thirdgraph.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-
-def nombre_moyen_par_solution(data):
-    print("Nombre moyen de CVE par solution")
+def nombre_cve_par_solution(data):
+    print("Nombre de CVE par solution")
     names = list(data.keys())
     all_values = list(data.values())
 
@@ -10,6 +10,11 @@ def nombre_moyen_par_solution(data):
     for matrix in all_values:
         cves.append(len(matrix))
 
-    plt.bar(range(len(data)), cves)
-    plt.legend(names)
+    cmap = cm.get_cmap('viridis', len(cves))
+    for i, (name, cve) in enumerate(zip(names, cves)):
+        plt.bar(i, cve, color=cmap(i), label=name)
+    plt.xticks(range(len(data)))
+    plt.xlabel("Solution")
+    plt.ylabel("Nombre CVE")
+    plt.legend()
     plt.show()
